9e329b565695f862c5ac501e2453e99f53ab4a2b
Matthias Percelay <matthias.percelay1@gmail.com>
2019-03-05 12:13:41 +0100

    comment out part broken by WIP

--- src/main/java/com/uca/nucas/engine/Automaton.java
+++ src/main/java/com/uca/nucas/engine/Automaton.java
@@ -6,7 +6,25 @@
 
 package com.uca.nucas.engine;
 
+import com.uca.nucas.engine.configuration.Configuration;
+import com.uca.nucas.engine.distribution.Distribution;
+
 public class Automaton {
     Alphabet alphabet;
     RuleSet ruleSet;
+
+    public Automaton(Alphabet alphabet, RuleSet ruleSet) {
+        this.alphabet = alphabet;
+        this.ruleSet = ruleSet;
+    }
+
+    /*public Configuration evaluate(Configuration conf, Distribution dist) {
+        int[] res = new int[conf.getSize()];
+
+        for (int i = 0; i < conf.getSize(); i++) {
+            res[i] = ruleSet.callRule(dist.get(i), i, conf);
+        }
+
+        return new Configuration(res);
+    }*/
 }
