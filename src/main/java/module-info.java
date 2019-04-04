f6873df2d5a730bcbb9bf78a7751159306241467
Matthias Percelay <matthias.percelay1@gmail.com>
2019-04-04 14:17:49 +0200

    remove obsolete dependency from module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module gui {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires afterburner.fx;
     opens com.uca.nucas.gui to javafx.fxml;
     exports com.uca.nucas.gui to javafx.graphics;
 
